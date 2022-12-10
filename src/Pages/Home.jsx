cff10b2c9d3477c98438f239bd14e72cb84d31c0
KPorus <designerfardin2021@gmail.com>
2022-12-10 12:50:53 +0600

    type edit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
-import Resume from "../asset/Resume.pdf"
+import React from "react";
+import { Typewriter } from "react-simple-typewriter";
+import Resume from "../asset/Resume.pdf";
 const Home = () => {
-    return (
-        <dv className='contaner'>
-            <div className='text-center mt-10 text-xl font-bold '>
-            <h1 className='py-2'>Hi I am Fardin Khan</h1>
-            <h1 className='py-2'>I am front-end web developer</h1>
-            <a href={Resume} download className='btn text-sm font-medium text-green-700'>Download Resume</a>
-            </div>
-        </dv>
-    );
+  return (
+    <dv className='contaner flex flex-col items-center justify-center relative top-[10rem]'>
+      <div className='text-center text-xl font-bold'>
+        <h1 className='py-4 text-5xl'>
+          Hi{""}
+          <br />
+          I am {""}
+          <span style={{ color: "green", fontWeight: "bold" }}>
+            <Typewriter
+              words={["Fardin Khan","Front-End web developer"]}
+              loop={5}
+              cursor
+              cursorStyle='_'
+              typeSpeed={100}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+          </span>
+        </h1>
+       
+      </div>
+      <a
+          href={Resume}
+          download
+          className='w-[25%] btn text-sm font-medium text-green-700'>
+          Download Resume
+        </a>
+    </dv>
+  );
 };
 
 export default Home;
