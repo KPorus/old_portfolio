69230c41eb86fb1a75519ced4710721e322541a9
KPorus <designerfardin2021@gmail.com>
2022-12-10 12:12:20 +0600

    download

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import Resume from "../asset/Resume.pdf"
 const Home = () => {
     return (
-        <div className='container'>
-            Home part
-        </div>
+        <dv className='contaner'>
+            <div className='text-center mt-10 text-xl font-bold '>
+            <h1 className='py-2'>Hi I am Fardin Khan</h1>
+            <h1 className='py-2'>I am front-end web developer</h1>
+            <a href={Resume} download className='btn text-sm font-medium text-green-700'>Download Resume</a>
+            </div>
+        </dv>
     );
 };
 
